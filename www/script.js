5203b28578fa283bfc86ff28ea650c43b1ee520e
Joakim Cars <joakim.cars@gmail.com>
2018-12-22 16:57:56 +0100

    fixed lock piece

--- www/script.js
+++ www/script.js
@@ -1,62 +1,59 @@
 window.onload = render
 
-let playerTurn = "white";
+let playerTurn = 'white'
 
-function renderBoard() {
-    const container = document.createElement('div')
-    container.classList.add('board')
+function renderBoard () {
+  const container = document.createElement('div')
+  container.classList.add('board')
 
-    
-    function getColor(x, y) {
-       return state[y*8 + x]
+  for (let y = 0; y < 8; ++y) {
+    const row = document.createElement('div')
+    row.classList.add('boardRow')
+    for (let x = 0; x < 8; ++x) {
+      const square = document.createElement('div')
+
+      square.classList.add('square')
+
+      const color = getColor(x, y)
+      if (color) {
+        const piece = document.createElement('div')
+        piece.classList.add(color === 'black' ? 'blackBrick' : 'whiteBrick')
+        square.appendChild(piece)
+      }
+      square.onclick = () => {
+        setState(x, y, playerTurn)
+      }
+      row.appendChild(square)
     }
-
-    for(let y = 0; y < 8; ++y) {
-        const row = document.createElement('div')
-        row.classList.add('boardRow')        
-        for(let x = 0; x < 8; ++x) {
-            const square = document.createElement('div')
-            
-            square.classList.add('square')
-
-            const color = getColor(x, y)
-            if(color) {
-                const piece = document.createElement('div')
-                piece.classList.add(color === 'black' ? 'blackBrick' : 'whiteBrick')
-                square.appendChild(piece)
-            }
-            square.onclick = () => {
-                setState(x, y, playerTurn)
-            }
-            row.appendChild(square)
-        }
-        container.appendChild(row)
-    }
-    return container
+    container.appendChild(row)
+  }
+  return container
 }
 
 const state = Array.from({ length: 64 }).fill(0)
 
+function getColor (x, y) {
+  return state[y * 8 + x]
+}
 
-function setState(x, y, s) {
-    state[y*8 + x] = s
+function setState (x, y, s) {
+  const occupied = getColor(x, y)
+  if (!occupied) {
+    state[y * 8 + x] = s
     render()
-    playerTurn = (playerTurn == "white") ? "black" : "white";
+    playerTurn = (playerTurn == 'white') ? 'black' : 'white'
+  }
 }
 
-function render() {
-    const root = document.getElementById('root')
-    const newBoard = renderBoard()
-    clear(root)
-    root.appendChild(newBoard)
+function render () {
+  const root = document.getElementById('root')
+  const newBoard = renderBoard()
+  clear(root)
+  root.appendChild(newBoard)
 }
 
-function clear(node) {
-    while (node.firstChild) {
-        node.removeChild(node.firstChild);
-    }    
-}
-
-function handleMove (squareId) {
-    alert(squareId);
+function clear (node) {
+  while (node.firstChild) {
+    node.removeChild(node.firstChild)
+  }
 }
